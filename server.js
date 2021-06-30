84e44f0d605599d501360c3d17ad39d904d9d1fd
Tony lewis <tonylewis353@gmail.com>
2021-06-30 14:24:51 +0100

    server message update - v1.26

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+/*******************************
+ *  Module: My own website
+ *  Date : 30/6/2021
+ *  Author: LJA
+ *  Version: v1.26
+ * 
+ * 
+ ********************************/
+
 var express = require("express");
 
 var app = express();
@@ -7,4 +16,5 @@ app.use(express.static('mywebsite'));
 var server = app.listen(8081, function(){
     var port = server.address().port;
     console.log("MyWebsite server started at http://localhost:%s", port);
+    console.log("Home Page  http://localhost:%s//home.html");
 });
